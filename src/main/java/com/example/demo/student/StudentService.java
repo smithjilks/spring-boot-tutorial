cf1bbcd4a5fdc4b1ac035830a429f4a7019b89b2
smithjilks <smithjilks@gmail.com>
2021-10-17 16:35:54 +0300

    Implement student update logic

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -37,5 +39,29 @@ public class StudentService {
                     "Student with id " + studentId + " does not exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"
+                ));
+
+        if (name != null && name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
