e234e8e0cec997a96e0fb1767a4e19cf3e782aca
smithjilks <smithjilks@gmail.com>
2021-10-17 15:27:06 +0300

    Add student respository dependency injection to student service

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,22 +1,21 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
     public List<Student> getStudents() {
-        return  List.of(
-                new Student(
-                        1L,
-                        "Jane",
-                        "jane.doe@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
