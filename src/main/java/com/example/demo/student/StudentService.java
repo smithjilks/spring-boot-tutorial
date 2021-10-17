b19b37b6d10995802a997d288adbd895691835a2
smithjilks <smithjilks@gmail.com>
2021-10-17 16:08:42 +0300

    Implement student post logic

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,18 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
+    @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-    @Autowired
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+
     }
 }
